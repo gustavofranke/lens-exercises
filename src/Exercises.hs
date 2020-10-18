a6f14b5326a85b715a8f862587206d736a86b85a
Gustavo Franke <franke.gustavo@gmail.com>
2020-10-18 22:29:57 +0100

    add some data types

---
+++ src/Exercises.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Exercises where
+
+import Data.Text
+import Control.Lens
+
+data User = User
+  { _name     :: Text
+  , _userid   :: Int
+  , _metadata :: UserInfo
+  }
+  deriving (Show)
+
+data UserInfo = UserInfo
+  { _numLogins     :: Int
+  , _associatedIPs :: [Text]
+  }
+  deriving (Show)
+
+makeLenses ''User
+makeLenses ''UserInfo
