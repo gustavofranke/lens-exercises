40ad167be0f0854e8304da7b5ba25cd7e5b8bbd5
Gustavo Franke <franke.gustavo@gmail.com>
2020-10-21 08:45:48 +0100

    add ex I

--- src/PrismsExercises.hs
+++ src/PrismsExercises.hs
@@ -4,8 +4,13 @@
 
 module PrismsExercises where
 
+import Control.Lens
 import Data.Aeson (Value)
+import Data.Aeson.Lens
 import Data.Aeson.QQ
+import qualified Data.Text as Text
+import Data.Text.Internal
+import Data.Vector
 
 -- :t key
 -- key
@@ -33,3 +38,40 @@ user2 =
     }
   }
 |]
+
+-- I
+a :: Maybe Integer
+a = user1 ^? key "metadata".key "num_logins"._Integer
+-- Nothing
+
+b :: Maybe Data.Text.Internal.Text
+b = user1 ^? key "email"._String
+-- Just "qyifan@xingxin.com"
+
+c :: Maybe Text
+c = user2 ^? key "email"._String
+-- Nothing
+
+d :: Maybe (Vector Value)
+d = user2 ^? key "metadata".key "associated_ips"._Array
+-- Nothing
+
+e :: Vector Value
+e = user2 ^. key "metadata".key "associated_ips"._Array
+-- []
+
+f :: Maybe Text
+f = user1 ^? key "name"._String.to Text.toUpper
+-- ust "QIAO.YIFAN"
+
+g :: Value
+g = user1 & key "name"._String .~ "su.mucheng"
+-- Object (fromList [("email",String "qyifan@xingxin.com"),("name",String "su.mucheng")])
+
+h :: Value
+h = user2 & key "email"._String .~ "yxiu@xingxin.com"
+-- Object (fromList [("name",String "ye.xiu"),("metadata",Object (fromList [("num_logins",Number 27.0)]))])
+
+i :: Value
+i = user2 & key "name"._String %~ Text.reverse
+-- Object (fromList [("name",String "uix.ey"),("metadata",Object (fromList [("num_logins",Number 27.0)]))])
